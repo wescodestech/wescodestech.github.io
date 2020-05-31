d2dbf2683c884a58fdbd839c5c986f3b3cc96853
Wesley R. Howell <wesleyrhowell@gmail.com>
2020-05-30 22:27:46 -0500

    Add shell application build

---
+++ precache-manifest.c0be60de7eb0b185e3c42fef46e9b7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103ad29f1c76564c271f94c926d6d744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f76223a8e5bc92d01c3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "721190ef49baf5bc9a59",
+    "url": "/static/js/2.03f6d9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03f6d9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f76223a8e5bc92d01c3",
+    "url": "/static/js/main.8f6ca74b.chunk.js"
+  },
+  {
+    "revision": "33e0abdcf6d74622c7b9",
+    "url": "/static/js/runtime-main.ababd92d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
